6c95be08569303378df6f0eaffab7f806c16db9e
Dan Spitz <spitz.dan.l@gmail.com>
2015-09-14 12:13:22 -0400

    install ipywidgets in install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-conda install --yes pandas pytables ipython ipython-notebook matplotlib scikit-learn joblib sqlalchemy psycopg2 pymysql odo 
+conda install --yes pandas pytables ipython ipython-notebook ipywidgets matplotlib scikit-learn joblib sqlalchemy psycopg2 pymysql odo 
